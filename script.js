70385a429b56c1ecadc205794253dca2b689a548
Arif Khan <144909236+arifkhan112006@users.noreply.github.com>
2024-02-03 19:54:45 +0500

    Add files via upload
    Updated

---
+++ script.js
@@ -0,0 +1,61 @@
+
+
+
+function question(){
+let a=prompt("4 + 4 :");
+if (a==8) {
+    alert( a + " " + "Good, Your Answer is Right.");
+    
+} else {
+    alert("Sorry, Your Answer is Wrong.");
+};
+
+}
+
+
+function question2(){
+    let a=prompt("2 + 7 :");
+    if (a==9) {
+        alert( a + " " + "Good, Your Answer is Right.");
+        
+    } else {
+        alert("Sorry, Your Answer is Wrong.");
+    };
+    
+    }
+
+
+    function question3(){
+        let a=prompt("1 + 5");
+        if (a==6) {
+            alert( a + " " + "Good, Your Answer is Right.");
+            
+        } else {
+            alert("Sorry, Your Answer is Wrong.");
+        };
+        
+        }
+    
+
+
+        function question4(){
+            let a=prompt("9 + 10");
+            if (a==19) {
+                alert( a + " " + "Good, Your Answer is Right.");
+                
+            } else {
+                alert("Sorry, Your Answer is Wrong.");
+            };
+            
+            }
+
+
+            function question5(){
+                let a=prompt("28 + 10");
+                if (a==38) {
+                    alert( a + " " + "Good, Your Answer is Right.");
+                } else {
+                    alert("Sorry, Your Answer is Wrong.");
+                };
+                
+                }
